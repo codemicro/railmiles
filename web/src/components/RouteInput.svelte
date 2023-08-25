65d7f939a930500e11fc8650942da3a85c82ffbd
AKP <abi@tdpain.net>
2023-08-25 13:10:57 +0100

    Remove extra padding

--- web/src/components/RouteInput.svelte
+++ web/src/components/RouteInput.svelte
@@ -15,19 +15,17 @@
     }
 </script>
 
-<div class="px-1">
-    {#each route as row, i}
-        <div class="input-group pb-1">
-            <input type="text" class="flex-item form-control" placeholder="Station" bind:value={route[i][0]}>
-            <input type="text" class="flex-item form-control" placeholder="Service ID (optional)"
-                   bind:value={route[i][1]}>
-            <button class="btn btn-sm btn-primary" on:click={(e) => addAtIndex(e, i+1)}>
-                <i class="bi-plus-lg"></i>
-            </button>
-            <button class="btn btn-sm btn-danger" disabled={i === route.length - 1 || i == 0}
-                    on:click={(e) => removeByIndex(e, i)}>
-                <i class="bi-trash3-fill"></i>
-            </button>
-        </div>
-    {/each}
-</div>
+{#each route as row, i}
+    <div class="input-group pb-1">
+        <input type="text" class="flex-item form-control" placeholder="Station" bind:value={route[i][0]}>
+        <input type="text" class="flex-item form-control" placeholder="Service ID (optional)"
+               bind:value={route[i][1]}>
+        <button class="btn btn-sm btn-primary" on:click={(e) => addAtIndex(e, i+1)}>
+            <i class="bi-plus-lg"></i>
+        </button>
+        <button class="btn btn-sm btn-danger" disabled={i === route.length - 1 || i == 0}
+                on:click={(e) => removeByIndex(e, i)}>
+            <i class="bi-trash3-fill"></i>
+        </button>
+    </div>
+{/each}
